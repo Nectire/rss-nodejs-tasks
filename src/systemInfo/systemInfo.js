318732c869f209fa2a482df9d2ee9e55be3622b8
Nectire <yazykovmaxim@gmail.com>
2022-06-12 20:24:33 +0300

    feat: add systemInfo module

---
+++ src/systemInfo/systemInfo.js
@@ -0,0 +1,16 @@
+
+import os from 'os';
+
+export const getCpus = () => {
+  return os.cpus().map((cpu) => {
+    return `Model: ${cpu.model}\nspeed: ${cpu.speed.toString().slice(0, 1)} GHz`;
+    }).join('\n') + `\ntotal cores (threads): ${os.cpus().length}`;
+}
+
+export const getSysUserName = () => os.userInfo().username;
+
+export const getArchitecture = () => os.arch();
+
+export const getHomeDir = () => os.homedir();
+
+export const getEOL = () => os.EOL;
